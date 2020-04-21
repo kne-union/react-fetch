8a1154c3d877c412cee1e3ef3628b4ad0027e90e
林志鹏 <zhipenglin@knx.com.cn>
2020-04-21 11:03:09 +0800

    添加refresh方法

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,38 +1,38 @@
-import { useCallback, useEffect, useImperativeHandle } from 'react';
+import {useCallback, useEffect, useImperativeHandle} from 'react';
 import useState from './useSafeState';
-import { globalParams } from './preset';
+import {globalParams} from './preset';
 
-export default ({ url, data, options }, ref) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [results, setResults] = useState({});
-  const [errorMsg, setErrorMsg] = useState();
-  const send = useCallback(() => {
-    setIsLoading(true);
-    return globalParams.ajax({
-      ...options,
-      url,
-      data
-    }).then((response) => {
-      const { data } = globalParams.transformResponse(response);
-      if (data.code === 200) {
-        setResults(data.results);
-      } else {
-        setErrorMsg(data.msg || '请求错误');
-      }
-    }).finally(() => {
-      setIsLoading(false);
-    });
-  }, [url, data, options]);
+export default ({url, data, options}, ref) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [results, setResults] = useState({});
+    const [errorMsg, setErrorMsg] = useState();
+    const send = useCallback(() => {
+        setIsLoading(true);
+        return globalParams.ajax({
+            ...options,
+            url,
+            data
+        }).then((response) => {
+            const {data} = globalParams.transformResponse(response);
+            if (data.code === 200) {
+                setResults(data.results);
+            } else {
+                setErrorMsg(data.msg || '请求错误');
+            }
+        }).finally(() => {
+            setIsLoading(false);
+        });
+    }, [url, data, options]);
 
-  useImperativeHandle(ref, () => {
-    return {
-      refresh: () => send()
-    };
-  }, [send]);
+    useImperativeHandle(ref, () => {
+        return {
+            refresh: () => send()
+        };
+    }, [send]);
 
-  useEffect(() => {
-    send();
-  }, [send]);
+    useEffect(() => {
+        send();
+    }, [send]);
 
-  return { isLoading, errorMsg, results };
+    return {isLoading, errorMsg, results, send};
 };
