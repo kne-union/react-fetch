bc26339bbb356d24a4e20ad9e372d96478883132
zhipenglin <sunandmoon001@163.com>
2023-04-25 18:58:34 +0800

    修改转换位置

--- src/plugins/transform-response.js
+++ src/plugins/transform-response.js
@@ -2,12 +2,11 @@ import {globalParams} from "../preset";
 
 export default {
     id: 'transform-response', plugin: (props, context) => {
-        const {transformResponse, transformData} = context.componentContext.getProps();
+        const {transformResponse} = context.componentContext.getProps();
         const response = context.outputStack['request'];
         if (!response) {
             return;
         }
-        const output = (transformResponse || globalParams.transformResponse)(Object.assign({}, response));
-        return typeof transformData === 'function' ? transformData(output) : output;
+        return (transformResponse || globalParams.transformResponse)(Object.assign({}, response));
     }, dependencies: ['request']
 };
