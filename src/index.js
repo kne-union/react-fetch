99d22bde1a76b2f42ffb0dba567d9ac615c379fc
zhipenglin <sunandmoon001@163.com>
2024-11-12 14:15:53 +0800

    添加getAjax

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ export {default} from './Fetch';
 export {default as Cache} from './Cache';
 export {default as useFetch} from './useFetch';
 export {default as withFetch, createWithFetch} from './withFetch';
-export {default as preset, getCache} from './preset';
+export {default as preset, getCache, getAjax} from './preset';
 export {default as request} from './request';
